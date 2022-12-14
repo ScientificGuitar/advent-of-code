540d1f06cd6ad0d185ee223a67cc3a1021c782b5
ScientificGuitar <jancoventer.lol@gmail.com>
2022-12-14 19:25:26 +0200

    Reformat day1

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,33 +1,14 @@
-def get_input():
-    """Get and parse the input."""
-    f = open("input/day1.txt", "r")
-    raw = f.read().strip()
-    calories = [
-        sum([int(calorie) for calorie in elf.split("\n")]) for elf in raw.split("\n\n")
-    ]
-    calories.sort()
-    return calories
+raw = open("2022/input/day1.txt", "r").read().strip()
+
+calories = [
+    sum([int(calorie) for calorie in elf.split("\n")]) for elf in raw.split("\n\n")
+]
+calories.sort()
 
 
-def part1(data):
-    """Solve part 1."""
-    return data[-1]
+def part_one():
+    return calories[-1]
 
 
-def part2(data):
-    """Solve part 2."""
-    return sum(data[-3:])
-
-
-def solve():
-    """Get input and solve the different days."""
-    data = get_input()
-    solution1 = part1(data)
-    solution2 = part2(data)
-    return solution1, solution2
-
-
-if __name__ == "__main__":
-    solutions = solve()
-    print("Solutions:")
-    print("\n".join(str(solution) for solution in solutions))
+def part_two():
+    return sum(calories[-3:])
